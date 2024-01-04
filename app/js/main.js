03f4cc148e2dd7f7639968c1932462d827f762fa
sergey-komar <kursev81@yandex.ru>
2024-01-04 21:38:17 +0300

    first client

--- app/js/main.js
+++ app/js/main.js
@@ -13,71 +13,124 @@ $(function () {
         centerPadding: '0',
         slidesToShow: 3,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+          
+          ]
        
     });
 
+
+   
+//qty-btn
+      $('.qty-btn').on('click', function (e) {
+        e.preventDefault();
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+                var newVal = parseFloat(oldValue) + 1;
+        } else {
+                // Don't allow decrementing below zero
+                if (oldValue > 0) {
+                        var newVal = parseFloat(oldValue) - 1;
+                } else {
+                        newVal = 0;
+                }
+        }
+        $button.parent().find('input').val(newVal);
+});
+
+ //Fancybox
+  
     Fancybox.bind("[data-fancybox]", {
         // Your custom options
       });
+  
+ 
 
 })
+
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
 
 //HEADER
     const header = document.querySelector('.header');
-    window.addEventListener('scroll', () => {
-       if(window.scrollY > 10){
-        header.classList.add('header-bg')
-       }else{
-        header.classList.remove('header-bg')
-       }
-    })
+
+    if(header){
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 10){
+             header.classList.add('header-bg')
+            }else{
+             header.classList.remove('header-bg')
+            }
+         })
+    }
+   
 
 
  // получаем все элементы
     const headingBlock = document.querySelector('.heading-block');
+    const headingVideo = document.querySelector('.heading__video');
     var videoEl = document.getElementsByTagName('video')[0],
     playBtn = document.getElementById('playBtn'),
     vidControls = document.getElementById('controls');
    
 
-    playBtn.addEventListener('click', () => {
-        headingBlock.classList.toggle('block-hidden');
-    })
+    if(playBtn){
+        playBtn.addEventListener('click', () => {
+            headingBlock.classList.toggle('block-hidden');
+        })
+    }
+  
 
-        // если браузер может воспроизводить видео удаляем класс
-        videoEl.addEventListener('canplaythrough', function () {
-        vidControls.classList.remove('hidden');
-        }, false);
-        // запускам или останавливаем воспроизведение
-        playBtn.addEventListener('click', function () {
-            if (videoEl.paused) {
-                videoEl.play();
-            } else {
-                videoEl.pause();
-            }
-            const path = videoEl.getAttribute('controls');
-            videoEl.setAttribute('controls', path);
+    if(videoEl && vidControls &&  playBtn && headingVideo){
+ // если браузер может воспроизводить видео удаляем класс
+ videoEl.addEventListener('canplaythrough', function () {
+    vidControls.classList.remove('hidden');
+    }, false);
+    // запускам или останавливаем воспроизведение
+    playBtn.addEventListener('click', function () {
+        if (videoEl.paused) {
+            videoEl.play();
+        } else {
+            videoEl.pause();
+        }
+        headingVideo.classList.toggle('hidden-video')
+        const path = videoEl.getAttribute('controls');
+        videoEl.setAttribute('controls', path);
 
-            }, false)
+        }, false)
+    }
+       
 
+        if(videoEl && playBtn){
+            videoEl.addEventListener('play', function () {
 
-        videoEl.addEventListener('play', function () {
-
-        playBtn.innerText = "";
-        }, false);
-
-        videoEl.addEventListener('pause', function () {
-
-        playBtn.innerText = "Play";
-        }, false);
-
+                playBtn.innerText = "";
+                }, false);
         
-        videoEl.addEventListener('ended', function () {
-        videoEl.currentTime = 0;
-        }, false);
+                videoEl.addEventListener('pause', function () {
+        
+                playBtn.innerText = "Play";
+                }, false);
+        }
+       
+
+        if(videoEl){
+            videoEl.addEventListener('ended', function () {
+                videoEl.currentTime = 0;
+                }, false);
+        }
+       
 
 
 
@@ -88,37 +141,44 @@ window.addEventListener('DOMContentLoaded', () => {
     playBtn1 = document.getElementById('playBtn-1')
     vidControls = document.getElementById('controls');
    
-    
-        // если браузер может воспроизводить видео удаляем класс
-        videoEl1.addEventListener('canplaythrough', function () {
-        vidControls.classList.remove('hidden');
-        }, false);
-        // запускам или останавливаем воспроизведение
-        playBtn1.addEventListener('click', function () {
-            if (videoEl1.paused) {
-                videoEl1.play();
-            } else {
-                videoEl1.pause();
-            }
-            const path = videoEl1.getAttribute('controls');
-            videoEl1.setAttribute('controls', path);
-            }, false)
+    if(videoEl1 && vidControls && playBtn1){
+ // если браузер может воспроизводить видео удаляем класс
+ videoEl1.addEventListener('canplaythrough', function () {
+    vidControls.classList.remove('hidden');
+    }, false);
+    // запускам или останавливаем воспроизведение
+    playBtn1.addEventListener('click', function () {
+        if (videoEl1.paused) {
+            videoEl1.play();
+        } else {
+            videoEl1.pause();
+        }
+        const path = videoEl1.getAttribute('controls');
+        videoEl1.setAttribute('controls', path);
+        }, false)
+    }
+       
 
 
+    if(videoEl1 && playBtn1){
         videoEl1.addEventListener('play', function () {
 
-        playBtn1.innerText = "";
-        }, false);
+            playBtn1.innerText = "";
+            }, false);
+    
+            videoEl1.addEventListener('pause', function () {
+    
+            playBtn1.innerText = "Play";
+            }, false);
+    }
+      
 
-        videoEl1.addEventListener('pause', function () {
-
-        playBtn1.innerText = "Play";
-        }, false);
-
-        
-        videoEl1.addEventListener('ended', function () {
-        videoEl1.currentTime = 0;
-        }, false);
+        if(videoEl1){
+            videoEl1.addEventListener('ended', function () {
+                videoEl1.currentTime = 0;
+                }, false);
+        }
+       
 
 
 
@@ -127,39 +187,59 @@ window.addEventListener('DOMContentLoaded', () => {
     playBtn = document.getElementById('playBtn-2'),
     vidControls = document.getElementById('controls');
    
+        if(videoEl2 &&  vidControls && playBtn){
+ // если браузер может воспроизводить видео удаляем класс
+ videoEl2.addEventListener('canplaythrough', function () {
+    vidControls.classList.remove('hidden');
+    }, false);
+    // запускам или останавливаем воспроизведение
+    playBtn.addEventListener('click', function () {
+        if (videoEl2.paused) {
+            videoEl2.play();
+        } else {
+            videoEl2.pause();
+        }
 
-        // если браузер может воспроизводить видео удаляем класс
-        videoEl2.addEventListener('canplaythrough', function () {
-        vidControls.classList.remove('hidden');
-        }, false);
-        // запускам или останавливаем воспроизведение
-        playBtn.addEventListener('click', function () {
-            if (videoEl2.paused) {
-                videoEl2.play();
-            } else {
-                videoEl2.pause();
-            }
+        const path = videoEl2.getAttribute('controls');
+        videoEl2.setAttribute('controls', path);
+        }, false)
 
-            const path = videoEl2.getAttribute('controls');
-            videoEl2.setAttribute('controls', path);
-            }, false)
+        }
+       
 
 
-        videoEl2.addEventListener('play', function () {
+        if(videoEl2 && playBtn){
+            videoEl2.addEventListener('play', function () {
 
-        playBtn.innerText = "";
-        }, false);
-
-        videoEl2.addEventListener('pause', function () {
-
-        playBtn.innerText = "Play";
-        }, false);
-
+                playBtn.innerText = "";
+                }, false);
         
-        videoEl2.addEventListener('ended', function () {
-        videoEl2.currentTime = 0;
-        }, false);
+                videoEl2.addEventListener('pause', function () {
+        
+                playBtn.innerText = "Play";
+                }, false);
+        }
+       
 
+        if(videoEl2){
+            videoEl2.addEventListener('ended', function () {
+                videoEl2.currentTime = 0;
+                }, false);
+        }
+      
+
+
+
+
+//MENU
+const menu = document.querySelector('.menu');
+const mobile = document.querySelector('.nav-icon');
+
+mobile.addEventListener('click', function(){
+    this.classList.toggle('nav-icon--active');
+    menu.classList.toggle('nav--active');
+
+});
 
 
 
